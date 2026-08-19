2fc5540674903069842a06bf6f0352bc2fc118ee
ece20becl09 <ece.20becl09@silicon.ac.in>
2026-08-19 21:53:11 +0530

    commit this change

--- CodePratice.py
+++ CodePratice.py
@@ -166,3 +166,12 @@ print(type(is_student))
 
 
 
+
+#Arithmetic operators:-
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: ")) 
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Remainder:", a % b)
